b88174a712dad16c403fc4aa678a115739d2520c
Nikhil Londhe <nikhillo@buffalo.edu>
2013-09-16 11:33:23 -0400

    Tests - wikipedia parser

--- src/edu/buffalo/cse/ir/wikiindexer/wikipedia/test/WikipediaParserTest.java
+++ src/edu/buffalo/cse/ir/wikiindexer/wikipedia/test/WikipediaParserTest.java
@@ -123,7 +123,8 @@ public class WikipediaParserTest {
 		//html escaping
 		assertEquals("Did you get me right?", WikipediaParser.parseTagFormatting("&lt;painful attr1='yes' attr2='no' &gt;Did you get me right?&lt;/pain&gt;"));
 		
-		/* TODO: Add more tests */
+		assertEquals("{{tl|cite book}}", WikipediaParser.parseTagFormatting("<nowiki>{{tl|cite book}}</nowiki>"));
+		
 	}
 
 	/**
@@ -131,15 +132,70 @@ public class WikipediaParserTest {
 	 */
 	@Test
 	public final void testParseTemplates() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("", WikipediaParser.parseTemplates("{{UK-singer-stub}}"));
+		assertEquals("", WikipediaParser.parseTemplates("{{YouTube|TnIpQhDn4Zg|Russ Conway playing Side Saddle}}"));
+		assertEquals("", WikipediaParser.parseTemplates("{{Unreferenced stub|auto=yes|date=December 2009}}"));
+		assertEquals("", WikipediaParser.parseTemplates("{{Reflist}}"));
+		assertEquals("", WikipediaParser.parseTemplates("{{Persondata |NAME = Kay, Kathie" +
+		"|ALTERNATIVE NAMES = Connie Wood" +
+		"|SHORT DESCRIPTION = Singer" +
+		"|DATE OF BIRTH     = 20 November 1918" +
+		"|PLACE OF BIRTH    = [[Gainsborough, Lincolnshire|Gainsborough]], [[Lincolnshire]], [[England]], UK" +
+		"|DATE OF DEATH     = 9 March 2005" +
+		"|PLACE OF DEATH    = [[Largs]], [[Ayrshire]], [[Scotland]], UK" +
+		"}}"));
 	}
 
 	/**
 	 * Test method for {@link edu.buffalo.cse.ir.wikiindexer.wikipedia.WikipediaParser#parseLinks(java.lang.String)}.
 	 */
+	@SuppressWarnings("deprecation")
 	@Test
 	public final void testParseLinks() {
-		fail("Not yet implemented"); // TODO
+		//null and empty checks
+		assertEquals(new Object[]{"",""}, WikipediaParser.parseLinks(""));
+		assertEquals(new Object[]{"",""}, WikipediaParser.parseLinks(null));
+		
+		//simple links
+		assertEquals(new Object[]{"Lone Star State","Texas"}, WikipediaParser.parseLinks("[[Texas|LOne Star State]]"));
+		
+		//auto capitalization
+		assertEquals(new Object[]{"London has public transport", "Public_transport"}, WikipediaParser.parseLinks("London has [[public transport]]"));
+		
+		//drop after _ and , automatically
+		assertEquals(new Object[]{"kingdom", "kingdom_(biology)"}, WikipediaParser.parseLinks("[[kingdom (biology)|]]"));
+		assertEquals(new Object[]{"Seattle", "Seattle,_Washington"}, WikipediaParser.parseLinks("[[Seattle, Washington|]]"));
+		
+		//outside namespace, not interested
+		assertEquals(new Object[]{"Village pump", ""}, WikipediaParser.parseLinks("[[Wikipedia:Village pump|]]"));
+		assertEquals(new Object[]{"Manual of Style", ""}, WikipediaParser.parseLinks("[[Wikipedia:Manual of Style (headings)|]]"));
+		assertEquals(new Object[]{"Wiktionary:Hello",""}, WikipediaParser.parseLinks("[[Wiktionary:Hello]]"));
+		assertEquals(new Object[]{"Wiktionary:fr:bonjour",""}, WikipediaParser.parseLinks("[[Wiktionary:fr:bonjour|]]"));
+		assertEquals(new Object[]{"Sound",""}, WikipediaParser.parseLinks("[[media:Classical guitar scale.ogg|Sound]]"));
+		assertEquals(new Object[]{"",""}, WikipediaParser.parseLinks("[[File:wiki.png]]"));
+		assertEquals(new Object[]{"Wikipedia encyclopedia",""}, WikipediaParser.parseLinks("[[File:wiki.png|right|Wikipedia encyclopedia]]"));
+		assertEquals(new Object[]{"Wikipedia logo",""}, WikipediaParser.parseLinks("[[File:wiki.png|frame|alt=Puzzle globe|Wikipedia logo]]"));
+												
+		
+		//blending etc.
+		assertEquals(new Object[]{"New York also has public transportation", "Public_transport"}, WikipediaParser.parseLinks("New York also has [[public transport|public transportation]]"));
+		assertEquals(new Object[]{"San Francisco also has public transportation", "Public_transport"}, WikipediaParser.parseLinks("San Francisco also has [[public transport]]ation"));
+		assertEquals(new Object[]{"A micro-second", "micro-"}, WikipediaParser.parseLinks("A [[micro-]]<nowiki />second."));
+		assertEquals(new Object[]{"Wikipedia:Manual of Style#Links",""}, WikipediaParser.parseLinks("[[Wikipedia:Manual of Style#Links|]]"));
+		
+		//categories: the method should parse 'em but not index
+		assertEquals(new Object[]{"Character sets",""}, WikipediaParser.parseLinks("[[Category:Character sets]]"));
+		
+		//same as above, but gos in the index not categories
+		assertEquals(new Object[]{"Category:Character sets",""}, WikipediaParser.parseLinks("[[:Category:Character sets]]"));
+		
+		//language links: parse but dont add to main index
+		assertEquals(new Object[]{"es:Plancton",""}, WikipediaParser.parseLinks("[[es:Plancton]]"));
+		assertEquals(new Object[]{"ru:Планктон",""}, WikipediaParser.parseLinks("[[ru:Планктон]]"));
+		
+		//external links
+		assertEquals(new Object[]{"Wikipedia",""}, WikipediaParser.parseLinks("[http://www.wikipedia.org Wikipedia]"));
+		assertEquals(new Object[]{"",""}, WikipediaParser.parseLinks("[http://www.wikipedia.org]"));
 	}
 
 }
